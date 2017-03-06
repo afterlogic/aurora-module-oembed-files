573596797627b67d8f7600712635db8d3d61360f
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:50:15 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\OEmbedFiles;
 
-class OEmbedFilesModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected $aProviders = array();
 	
